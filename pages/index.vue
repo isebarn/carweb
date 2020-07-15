29509661ab1a007e97bfe6f9bd4ce53732e6f1a6
isebarn <isebarn182@gmail.com>
2020-07-15 18:12:16 +0000

    Some data analysis beneath chart

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,39 @@
 <template>
   <v-container fill-height fluid>
     <v-row ref="table" justify="center" align="center">
-      <v-col cols="4">
-        <v-data-table
-          :headers="headers"
-          :items="allItems"
-          :sort-by.sync="sortBy"
-          :sort-desc.sync="sortDesc"
-          @click:row="allItemsRowClick($event)"
-        />
-      </v-col>
-      <v-col cols="8">
-        <bar-chart v-if="showChart" :key="idx" :data="barChartData" :options="barChartOptions" />
+      <v-col cols="12">
+        <v-card>
+          <v-layout row wrap>
+            <v-col cols="4">
+              <v-data-table
+                :headers="headers"
+                :items="allItems"
+                :sort-by.sync="sortBy"
+                :sort-desc.sync="sortDesc"
+                @click:row="allItemsRowClick($event)"
+              />
+            </v-col>
+            <v-col v-if="showChart" cols="8">
+              <v-col cols="12">
+                <bar-chart :key="idx" :data="barChartData" :options="barChartOptions" />
+              </v-col>
+              <v-col cols="6">
+                <v-list-item>
+                  <v-list-item-title>No. sold </v-list-item-title>
+                  <v-list-item-subtitle class="text-right">
+                    {{ numSold }}
+                  </v-list-item-subtitle>
+                </v-list-item>
+                <v-list-item v-if="avgDaysToSell">
+                  <v-list-item-title>Avg. days to sell </v-list-item-title>
+                  <v-list-item-subtitle class="text-right">
+                    {{ avgDaysToSell }}
+                  </v-list-item-subtitle>
+                </v-list-item>
+              </v-col>
+            </v-col>
+          </v-layout>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -19,6 +41,7 @@
 
 <script>
 
+import { mean } from 'mathjs'
 import BarChart from '~/components/BarChart'
 
 export default {
@@ -36,6 +59,8 @@ export default {
     return {
       idx: 0,
       showChart: false,
+      numSold: null,
+      avgDaysToSell: null,
 
       windowSize: {
         x: 0,
@@ -114,6 +139,19 @@ export default {
       this.barChartData.datasets[0].data = []
       const { data } = await this.$api.Data.getByMakerModelYear(event.Maker, event.Model, event.Year)
 
+      // Get no. cars sold
+      this.numSold = data.filter(x => x.Sold).length
+
+      // Get avg days to sell car
+      const dateDiffMillisec = data.filter(x => x.Sold).map(y => new Date(y.Sold) - new Date(y.Created))
+      const dateDiffDays = dateDiffMillisec.map(x => Math.floor(x / (1000 * 60 * 60 * 24)))
+      const dateDiffViable = dateDiffDays.filter(x => x <= 31)
+
+      this.avgDaysToSell = dateDiffViable.length > 2
+        ? Math.floor(mean(dateDiffViable))
+        : null
+
+      // create table data
       const result = []
       data.forEach(car => result.push({ y: car.Price / 1000, x: car.Driven / 1000, data: car }))
       this.barChartData.datasets[0].pointBackgroundColor = data.map(x => x.Sold ? 'red' : 'green')
